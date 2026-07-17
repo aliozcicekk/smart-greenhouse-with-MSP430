ef6f356f51e15b4615c65a3c39a3a6b3154e29ba
Ali Özçiçek <ozcicekali1@gmail.com>
2026-07-17 14:59:00 +0300

    Update ssd1306.c

--- ssd1306.c
+++ ssd1306.c
@@ -1,68 +1,66 @@
 #include "ssd1306.h"
 
-unsigned char buffer[17];                                       
+unsigned char buffer[17];                                        
 const unsigned char HcenterUL[] = {                                      
                                0,                                      
                                61,                                     
                                58,                                      
-                               55,                                   
+                               55,                                    
                                49,                                  
                                46,                                 
                                43,                                      
-                               37,                               
-                               34,           
+                               37,                                
+                               34,            
                                31,                                      
                                25                                       
 };
 
 void ssd1306_init(void) {
-    // SSD1306 init sequence
-    ssd1306_command(SSD1306_DISPLAYOFF);                              
-    ssd1306_command(SSD1306_SETDISPLAYCLOCKDIV);                      
-    ssd1306_command(0x80);                                             
+    ssd1306_command(SSD1306_DISPLAYOFF);              
+    ssd1306_command(SSD1306_SETDISPLAYCLOCKDIV);      
+    ssd1306_command(0x80);                                              
 
-    ssd1306_command(SSD1306_SETMULTIPLEX);                           
+    ssd1306_command(SSD1306_SETMULTIPLEX);            
     ssd1306_command(SSD1306_LCDHEIGHT - 1);
 
-    ssd1306_command(SSD1306_SETDISPLAYOFFSET);                          // 0xD3
-    ssd1306_command(0x0);                                               // no offset
-    ssd1306_command(SSD1306_SETSTARTLINE | 0x0);                        // line #0
-    ssd1306_command(SSD1306_CHARGEPUMP);                                // 0x8D
-    ssd1306_command(0x14);                                             
-    ssd1306_command(SSD1306_MEMORYMODE);                            
+    ssd1306_command(SSD1306_SETDISPLAYOFFSET);
+    ssd1306_command(0x0);
+    ssd1306_command(SSD1306_SETSTARTLINE | 0x0);
+    ssd1306_command(SSD1306_CHARGEPUMP);
+    ssd1306_command(0x14);
+    ssd1306_command(SSD1306_MEMORYMODE);            
     ssd1306_command(0x00);                                        
     ssd1306_command(SSD1306_SEGREMAP | 0x1);
     ssd1306_command(SSD1306_COMSCANDEC);
 
-    ssd1306_command(SSD1306_SETCOMPINS);                         
+    ssd1306_command(SSD1306_SETCOMPINS);          
     ssd1306_command(0x12);
-    ssd1306_command(SSD1306_SETCONTRAST);                               // 0x81
+    ssd1306_command(SSD1306_SETCONTRAST);
     ssd1306_command(0xCF);
 
-    ssd1306_command(SSD1306_SETPRECHARGE);                              // 0xd9
+    ssd1306_command(SSD1306_SETPRECHARGE);
     ssd1306_command(0xF1);
-    ssd1306_command(SSD1306_SETVCOMDETECT);                             // 0xDB
+    ssd1306_command(SSD1306_SETVCOMDETECT);
     ssd1306_command(0x40);
-    ssd1306_command(SSD1306_DISPLAYALLON_RESUME);                       // 0xA4
-    ssd1306_command(SSD1306_NORMALDISPLAY);                             // 0xA6
+    ssd1306_command(SSD1306_DISPLAYALLON_RESUME);
+    ssd1306_command(SSD1306_NORMALDISPLAY);
 
     ssd1306_command(SSD1306_DEACTIVATE_SCROLL);
 
-    ssd1306_command(SSD1306_DISPLAYON);                                 //--turn on oled panel
-} // end ssd1306_init
+    ssd1306_command(SSD1306_DISPLAYON);
+}
 
 void ssd1306_command(unsigned char command) {
     buffer[0] = 0x80;
     buffer[1] = command;
 
     i2c_write(SSD1306_I2C_ADDRESS, buffer, 2);
-} // end ssd1306_command
+}
 
 void ssd1306_clearDisplay(void) {
-
     ssd1306_setPosition(0, 0);
     uint8_t i;
-    for (i = 64; i > 0; i--) {                                          // count down for loops when possible for ULP
+    for (i = 64; i > 0; i--) {
         uint8_t x;
         for(x = 16; x > 0; x--) {
             if (x == 1) {
@@ -74,25 +72,25 @@ void ssd1306_clearDisplay(void) {
 
         i2c_write(SSD1306_I2C_ADDRESS, buffer, 17);
     }
-} // end ssd1306_clearDisplay
+}
 
 void ssd1306_setPosition(uint8_t column, uint8_t page) {
     if (column > 128) {
-        column = 0;                                                     // constrain column to upper limit
+        column = 0;
     }
 
     if (page > 8) {
-        page = 0;                                                       // constrain page to upper limit
+        page = 0;
     }
 
     ssd1306_command(SSD1306_COLUMNADDR);
-    ssd1306_command(column);                                            // Column start address (0 = reset)
-    ssd1306_command(SSD1306_LCDWIDTH-1);                                // Column end address (127 = reset)
+    ssd1306_command(column);
+    ssd1306_command(SSD1306_LCDWIDTH-1);
 
     ssd1306_command(SSD1306_PAGEADDR);
-    ssd1306_command(page);                                              // Page start address (0 = reset)
-    ssd1306_command(7);                                                 // Page end address
-} // end ssd1306_setPosition
+    ssd1306_command(page);
+    ssd1306_command(7);
+}
 
 void ssd1306_printText(uint8_t x, uint8_t y, char *ptString) {
     ssd1306_setPosition(x, y);
@@ -100,10 +98,10 @@ void ssd1306_printText(uint8_t x, uint8_t y, char *ptString) {
     while (*ptString != '\0') {
         buffer[0] = 0x40;
 
-        if ((x + 5) >= 127) {                                           // char will run off screen
-            x = 0;                                                      // set column to 0
-            y++;                                                        // jump to next page
-            ssd1306_setPosition(x, y);                                  // send position change to oled
+        if ((x + 5) >= 127) {
+            x = 0;
+            y++;
+            ssd1306_setPosition(x, y);
         }
 
         uint8_t i;
@@ -117,7 +115,7 @@ void ssd1306_printText(uint8_t x, uint8_t y, char *ptString) {
         ptString++;
         x+=6;
     }
-} // end ssd1306_printText
+}
 
 void ssd1306_printTextBlock(uint8_t x, uint8_t y, char *ptString) {
     char word[12];
@@ -134,13 +132,13 @@ void ssd1306_printTextBlock(uint8_t x, uint8_t y, char *ptString) {
                 endX += 6;
             }
 
-            word[i++] = '\0'; //++ for line 25 to work as space would leave endX to remain zero
+            word[i++] = '\0';
 
             if (endX >= 127) {
                 x = 0;
                 y++;
                 if (y > 7)
-                    break; //prevent overflow
+                    break;
                 ssd1306_printText(x, y, word);
                 endX = (i * 6);
                 x = endX;
@@ -150,16 +148,14 @@ void ssd1306_printTextBlock(uint8_t x, uint8_t y, char *ptString) {
                 x = endX;
             }
 
-            //either space or null broke second while. Prevent pt from skipping null if so.
             if (i <= 10 && *ptString != '\0') {
                 ptString++;
-            } else if ((endX - 6) >= 0) { //buffer(char word[12]) filled, reverse the spacing (line 29 & 30)
-                endX -= 6;              //prevent underflow
+            } else if ((endX - 6) >= 0) {
+                endX -= 6;
                 x = endX;
             }
         } while (*ptString != '\0');
 }
-
 
 void ssd1306_printUI32( uint8_t x, uint8_t y, uint32_t val, uint8_t Hcenter ) {
     char text[14];
@@ -170,7 +166,7 @@ void ssd1306_printUI32( uint8_t x, uint8_t y, uint32_t val, uint8_t Hcenter ) {
     } else {
         ssd1306_printText(x, y, text);
     }
-} // end ssd1306_printUI32
+}
 
 uint8_t ssd1306_digits(uint32_t n) {
     if (n < 10) {
@@ -194,24 +190,24 @@ uint8_t ssd1306_digits(uint32_t n) {
     } else {
         return 10;
     }
-} // end digits
+}
 
 void ssd1306_ultoa(uint32_t val, char *string) {
     uint8_t i = 0;
     uint8_t j = 0;
-                                                                        // use do loop to convert val to string
+
     do {
-        if (j==3) {                                                     // we have reached a separator position
-            string[i++] = ',';                                          // add a separator to the number string
-            j=0;                                                        // reset separator indexer thingy
+        if (j==3) {
+            string[i++] = ',';
+            j=0;
         }
-            string[i++] = val%10 + '0';                                 // add the ith digit to the number string
-            j++;                                                        // increment counter to keep track of separator placement
+        string[i++] = val%10 + '0';
+        j++;
     } while ((val/=10) > 0);
 
-    string[i++] = '\0';                                                 // add termination to string
-    ssd1306_reverse(string);                                                    // string was built in reverse, fix that
-} // end ultoa
+    string[i++] = '\0';
+    ssd1306_reverse(string);
+}
 
 void ssd1306_reverse(char *s)
 {
@@ -223,4 +219,4 @@ void ssd1306_reverse(char *s)
         s[i] = s[j];
         s[j] = c;
     }
-} // end reverse
+}
